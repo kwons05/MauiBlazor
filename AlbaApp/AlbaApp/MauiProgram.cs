6437d8cf8ad4c1f0b422319ac72c90a067e22cdb
kwons <code.sona@gmail.com>
2025-01-31 15:51:51 +0900

    init

---
+++ AlbaApp/AlbaApp/MauiProgram.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using Radzen;
+
+namespace AlbaApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+            // Radzen Builder Start After [AddMauiBlazorWebView]
+            builder.Services.AddRadzenComponents();
+            // Radzen Builder End
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
